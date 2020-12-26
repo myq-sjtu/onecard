77de3367178815c25a67137ebcc2675b10ff75e9
麦钰麒520021910367 <yq-mai@sjtu.edu.cn>
2020-12-26 14:12:20 +0800

    Dec26

---
+++ card.cpp
@@ -0,0 +1,94 @@
+#include"card.h"
+
+using namespace std;
+
+void card::printfunc(){
+    switch(color){
+        case 0:{
+            cout<<"万能牌 ";
+            break;
+        }
+        case 1:{
+            cout<<"红 ";
+            break;
+        }
+        case 2:{
+            cout<<"蓝 ";
+            break;
+        }
+        case 3:{
+            cout<<"绿 ";
+            break;
+        }
+        case 4:{
+            cout<<"黄 ";
+            break;
+        }
+        default: break;
+    }
+    switch(number){
+        case 0:{
+            cout<<"+2 ";
+            break;
+        }
+        case 1:{
+            cout<<"转向 ";
+            break;
+        }
+        case 2:{
+            cout<<"+4 ";
+            break;
+        }
+        case 3:{
+            cout<<"变色 ";
+            break;
+        }
+        default: break;
+    }
+}
+
+void card::printnum(){
+    switch(color){
+        case 0:{
+            cout<<"万能牌 ";
+            break;
+        }
+        case 1:{
+            cout<<"红 ";
+            break;
+        }
+        case 2:{
+            cout<<"蓝 ";
+            break;
+        }
+        case 3:{
+            cout<<"绿 ";
+            break;
+        }
+        case 4:{
+            cout<<"黄 ";
+            break;
+        }
+        default: break;
+    }
+    cout<<number<<' ';
+}
+
+card::card(int a,int b, int c){
+    type=a;
+    color=b;
+    number=c;
+}
+
+void card::printcard(){
+    switch(type){
+        case 0:{
+            printfunc();
+            break;
+        }
+        case 1:{
+            printnum();
+            break;
+        }
+    }
+}
--- card.h
+++ card.h
@@ -4,11 +4,18 @@
 #include<vector>
 #include<iostream>
 
+using namespace std;
+
 class card{
 private:
     int type;//0 is functional,1 is number
     int color;// 0 for wan neng pai;
     int number;//for functional, 0
 public:
-    card
-}
+    card(int,int,int);
+    void printcard();
+    void printfunc();
+    void printnum();
+};
+
+#endif
