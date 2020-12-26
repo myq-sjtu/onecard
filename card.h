c2f5c5e71185d12b6981988bfeb37614fbdaa8ea
myq-sjtu <2331029011@qq.com>
2020-12-26 14:00:17 +0800

    DEC26-1

---
+++ card.h
@@ -0,0 +1,14 @@
+#ifndef CARD_H
+#define CARD_H
+
+#include<vector>
+#include<iostream>
+
+class card{
+private:
+    int type;//0 is functional,1 is number
+    int color;// 0 for wan neng pai;
+    int number;//for functional, 0
+public:
+    card
+}
